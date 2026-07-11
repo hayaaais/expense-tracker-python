7dfe40ae240440bee52c9a8cf932b356d0172abd
hayaaais <galymjankyzy.aa@gmail.com>
2026-07-11 22:30:10 +0500

    feat: add expense reports and analytics

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def show_menu():
     print("3. Delete expense")
     print("4. Show total")
     print("5. Search expenses")
-    print("6. Exit")
+    print("6. Reports")
+    print("7. Exit")
 
 expenses = read_expenses()
 
@@ -95,64 +96,138 @@ def delete_expense():
 
 def show_total():
     total = sum(exp['amount'] for exp in expenses)
-    print(f"\nYour total is {total}₸\n")
+    print(f"\nYour total is {total:.2f}₸\n")
 
 
 def search_expenses():
-    if not expenses:
-        print("\nNo expenses to search.\n")
-        return
-    
-    print("\nSearch by:")
-    print("1. Category")
-    print("2. Date")
-    print("3. Description")
-    print("4. Back")
-    srch_expenses = []
-
     def find_expense(exp, field, value, partial_match):
-        if partial_match:
-            if value in exp[field].lower():
-                srch_expenses.append(exp)
+            if partial_match:
+                if value in exp[field].lower():
+                    srch_expenses.append(exp)
+            else:
+                if exp[field] == value:
+                    srch_expenses.append(exp)
+
+    while True:
+        if not expenses:
+            print("\nNo expenses to search.\n")
+            return
+        
+        print("\nSearch by:")
+        print("1. Category")
+        print("2. Date")
+        print("3. Description")
+        print("4. Back")
+        choice = input("\nChoose an option: ")
+        srch_expenses = []
+
+        if choice == "1":
+            field, partial_match = "category", False
+            value = input("\nEnter category: ").title()
+        elif choice == "2":
+            field, partial_match = "date", False
+            value = input("\nEnter date (YYYY-MM-DD, leave blank for today): ").strip()
+            if not value:
+                value = datetime.date.today().strftime("%Y-%m-%d")
+            else:
+                try:
+                    datetime.datetime.strptime(value, "%Y-%m-%d")
+                except ValueError:
+                    print("Invalid format! Please use YYYY-MM-DD.\n")
+                    return
+        elif choice == "3":
+            field, partial_match = "description", True
+            value = input("\nEnter keyword for description: ").lower()
+        elif choice == "4":
+            break
         else:
-            if exp[field] == value:
-                srch_expenses.append(exp)
+            print("\nInvalid input! Going back now - Try again\n")
+            continue
 
-    choice = input("\nChoose an option: ")
-
-    if choice == "1":
-        field = "category"
-        partial_match = False
-        value = input("\nEnter category: ").title()
-    elif choice == "2":
-        field = "date"
-        partial_match = False
-        value = input("\nEnter date (YYYY-MM-DD, leave blank for today): ").strip()
         if not value:
-            value = datetime.date.today().strftime("%Y-%m-%d")
+            print("Search cannot be empty.")
+            return
         else:
-            try:
-                datetime.datetime.strptime(value, "%Y-%m-%d")
-            except ValueError:
-                print("Invalid format! Please use YYYY-MM-DD.\n")
-                return
-    elif choice == "3":
-        field = "description"
-        partial_match = True
-        value = input("\nEnter keyword for description: ").lower()
-    elif choice == "4":
-        return
-    else:
-        print("\nInvalid input! Going back now - Try again\n")
-        return
+            for exp in expenses:
+                find_expense(exp, field, value, partial_match)
 
-    for exp in expenses:
-        find_expense(exp, field, value, partial_match)
+        if not srch_expenses:
+            print("\nNo matching expenses found.\n")
+        else:
+            print_expenses(srch_expenses)
 
-    if not srch_expenses:
-        print("\nNo matching expenses found.\n")
-    else:
-        print_expenses(srch_expenses)
+
+def get_category_totals(expenses):
+    totals_dict = {}
+    available_categories = set(exp["category"] for exp in expenses)
+    for ctg in sorted(available_categories):
+        ctg_list = []
+        for exp in expenses:
+            if exp["category"] == ctg:
+                ctg_list.append(exp)
+        if ctg_list:
+            category_total = sum(exp['amount'] for exp in ctg_list)
+            totals_dict[ctg] = category_total
+    return totals_dict
+
+
+def show_reports():
+    def print_extreme_expenses(expenses, mode="highest"):
+        is_reverse = True if mode == "highest" else False
+        sorted_expenses = sorted(expenses, key=lambda x: x["amount"], reverse=is_reverse)
+        target_amount = sorted_expenses[0]["amount"]
+        print(f"\n{mode.title()} Expense(s)")
+        for exp in sorted_expenses:
+            if exp["amount"] == target_amount:
+                print(f"{exp['amount']}₸\n{exp['category']}\n{exp['description']}\n{exp['date']}\n---")
+            else:
+                break
+
+    while True:
+        if not expenses:
+            print("No expenses recorded yet.")
+            return
+
+        print("\nReports")
+        print("1. Spending by category")
+        print("2. Highest expense")
+        print("3. Lowest expense")
+        print("4. Average expense")
+        print("5. Expense summary")
+        print("6. Back")
+        choice = input("\nChoose an option: ")
+
+        if choice == "1":
+            print("\nSpending by category\n")
+            category_data = get_category_totals(expenses)
+            for ctg, total in category_data.items():
+                print(f"{ctg.ljust(15, '.')} {total:.2f}₸")
+            print()        
+        elif choice == "2":
+            print_extreme_expenses(expenses, mode="highest")
+        elif choice == "3":
+            print_extreme_expenses(expenses, mode="lowest")
+        elif choice == "4":
+            print("\nAverage expense\n")
+            total_spent = sum(exp['amount'] for exp in expenses)
+            average = total_spent / len(expenses)
+            print(f"Your average expense amount is: {average:.2f}₸")
+        elif choice == "5":
+            total_spent = sum(exp['amount'] for exp in expenses)
+            average = total_spent / len(expenses)
+            highest_expense = max(exp["amount"] for exp in expenses)
+            lowest_expense = min(exp["amount"] for exp in expenses)
+            print("\nSummary\n")
+            print(f"Total expenses: {len(expenses)}\n")
+            print(f"Total spent: {total_spent}₸\n")
+            print(f"Average expense: {average}₸\n")
+            print(f"Highest expense: {highest_expense}₸\n")
+            print(f"Lowest expense: {lowest_expense}₸\n")
+        elif choice == "6":
+            break
+        else:
+            print("\nInvalid input! Going back now - Try again\n")
+            continue
 
 
 while True:
@@ -176,7 +251,11 @@ while True:
         search_expenses()
 
     elif choice == "6":
+        show_reports()
+
+    elif choice == "7":
         print("\nExiting tracker. Goodbye!")
         break
     else:
-        print("\nPlease choose option from 1 to 5\n")
+        print("\nPlease choose option from 1 to 8\7\n")
+        continue
