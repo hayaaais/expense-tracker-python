51119108a08e1576f708a4837ab410a41943db01
hayaaais <galymjankyzy.aa@gmail.com>
2026-07-12 21:37:29 +0500

    refactor: move search_expenses function to search.py and update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 from storage import read_expenses, save_expenses, load_budget, save_budget
 from reports import show_reports, get_total_spent, get_category_totals
 from sorting import sorting
+from search import search_expenses
 
 print("Expense Tracker")
 
@@ -104,65 +105,6 @@ def show_total():
     print(f"\nYour total is {total:.2f}₸\n")
 
 
-def search_expenses():
-    def find_expense(exp, field, value, partial_match):
-            if partial_match:
-                if value in exp[field].lower():
-                    srch_expenses.append(exp)
-            else:
-                if exp[field] == value:
-                    srch_expenses.append(exp)
-
-    while True:
-        if not expenses:
-            print("\nNo expenses to search.\n")
-            return
-        
-        print("\nSearch by:")
-        print("1. Category")
-        print("2. Date")
-        print("3. Description")
-        print("4. Back")
-        choice = input("\nChoose an option: ")
-        srch_expenses = []
-
-        if choice == "1":
-            field, partial_match = "category", False
-            value = input("\nEnter category: ").title()
-        elif choice == "2":
-            field, partial_match = "date", False
-            value = input("\nEnter date (YYYY-MM-DD, leave blank for today): ").strip()
-            if not value:
-                value = datetime.date.today().strftime("%Y-%m-%d")
-            else:
-                try:
-                    datetime.datetime.strptime(value, "%Y-%m-%d")
-                except ValueError:
-                    print("Invalid format! Please use YYYY-MM-DD.\n")
-                    return
-        elif choice == "3":
-            field, partial_match = "description", True
-            value = input("\nEnter keyword for description: ").lower()
-        elif choice == "4":
-            print()
-            break
-        else:
-            print("\nInvalid input! Going back now - Try again\n")
-            continue
-
-        if not value:
-            print("Search cannot be empty.")
-            return
-        else:
-            for exp in expenses:
-                find_expense(exp, field, value, partial_match)
-
-        if not srch_expenses:
-            print("\nNo matching expenses found.\n")
-        else:
-            print_expenses(srch_expenses)
-
-
 def get_monthly_expenses(month):
     monthly_expenses = []
     for exp in expenses:
@@ -254,7 +196,7 @@ while True:
         show_total()
 
     elif choice == "5":
-        search_expenses()
+        search_expenses(expenses)
 
     elif choice == "6":
         show_reports(expenses)
---
+++ search.py
@@ -0,0 +1,61 @@
+import json
+import datetime
+from main import print_expenses
+
+def find_expense(exp, field, value, partial_match, srch_expenses):
+            if partial_match:
+                if value in exp[field].lower():
+                    srch_expenses.append(exp)
+            else:
+                if exp[field] == value:
+                    srch_expenses.append(exp)
+
+def search_expenses(expenses):
+    while True:
+        if not expenses:
+            print("\nNo expenses to search.\n")
+            return
+        
+        print("\nSearch by:")
+        print("1. Category")
+        print("2. Date")
+        print("3. Description")
+        print("4. Back")
+        choice = input("\nChoose an option: ")
+        srch_expenses = []
+
+        if choice == "1":
+            field, partial_match = "category", False
+            value = input("\nEnter category: ").title()
+        elif choice == "2":
+            field, partial_match = "date", False
+            value = input("\nEnter date (YYYY-MM-DD, leave blank for today): ").strip()
+            if not value:
+                value = datetime.date.today().strftime("%Y-%m-%d")
+            else:
+                try:
+                    datetime.datetime.strptime(value, "%Y-%m-%d")
+                except ValueError:
+                    print("Invalid format! Please use YYYY-MM-DD.\n")
+                    return
+        elif choice == "3":
+            field, partial_match = "description", True
+            value = input("\nEnter keyword for description: ").lower()
+        elif choice == "4":
+            print()
+            break
+        else:
+            print("\nInvalid input! Going back now - Try again\n")
+            continue
+
+        if not value:
+            print("Search cannot be empty.")
+            return
+        else:
+            for exp in expenses:
+                find_expense(exp, field, value, partial_match, srch_expenses)
+
+        if not srch_expenses:
+            print("\nNo matching expenses found.\n")
+        else:
+            print_expenses(srch_expenses)
