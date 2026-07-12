d7539bdc8b9c5c651b233996888c25258e5a1703
hayaaais <galymjankyzy.aa@gmail.com>
2026-07-12 21:34:14 +0500

    added sorting.py and moved functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 from storage import read_expenses, save_expenses, load_budget, save_budget
 from reports import show_reports, get_total_spent, get_category_totals
+from sorting import sorting
 
 print("Expense Tracker")
 
@@ -160,48 +161,6 @@ def search_expenses():
             print("\nNo matching expenses found.\n")
         else:
             print_expenses(srch_expenses)
-
-
-def sorting():
-    def sort_expenses(expenses, field, reverse):
-            return sorted(expenses, key=lambda x: x[field], reverse=reverse)
-    
-    while True:
-        if not expenses:
-            print("No expenses found to sort.")
-            return
-        
-        print("\nSorting")
-        print("1. Amount ↑")
-        print("2. Amount ↓")
-        print("3. Date ↑")
-        print("4. Date ↓")
-        print("5. Category")
-        print("6. Description (A–Z)")
-        print("7. Back")
-        choice = input("\nChoose an option: ")
-
-        if choice == "1":
-            field, reverse = "amount", False
-        elif choice == "2":
-            field, reverse = "amount", True
-        elif choice == "3":
-            field, reverse = "date", False
-        elif choice == "4":
-            field, reverse = "date", True
-        elif choice == "5":
-            field, reverse = "category", False
-        elif choice == "6":
-            field, reverse = "description", False
-        elif choice == "7":
-            print()
-            break
-        else:
-            print("\nInvalid input! Going back now - Try again\n")
-            continue
-        
-        sorted_expenses = sort_expenses(expenses, field, reverse)
-        print_expenses(sorted_expenses)
 
 
 def get_monthly_expenses(month):
---
+++ sorting.py
@@ -0,0 +1,44 @@
+import json
+from main import print_expenses
+
+def sort_expenses(expenses, field, reverse):
+            return sorted(expenses, key=lambda x: x[field], reverse=reverse)
+
+def sorting(expenses):
+    
+    while True:
+        if not expenses:
+            print("No expenses found to sort.")
+            return
+        
+        print("\nSorting")
+        print("1. Amount ↑")
+        print("2. Amount ↓")
+        print("3. Date ↑")
+        print("4. Date ↓")
+        print("5. Category")
+        print("6. Description (A–Z)")
+        print("7. Back")
+        choice = input("\nChoose an option: ")
+
+        if choice == "1":
+            field, reverse = "amount", False
+        elif choice == "2":
+            field, reverse = "amount", True
+        elif choice == "3":
+            field, reverse = "date", False
+        elif choice == "4":
+            field, reverse = "date", True
+        elif choice == "5":
+            field, reverse = "category", False
+        elif choice == "6":
+            field, reverse = "description", False
+        elif choice == "7":
+            print()
+            break
+        else:
+            print("\nInvalid input! Going back now - Try again\n")
+            continue
+        
+        sorted_expenses = sort_expenses(expenses, field, reverse)
+        print_expenses(sorted_expenses)
